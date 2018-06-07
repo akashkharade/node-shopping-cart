4d90e1bbc45b8943c568bccdad9c07b587193dd8
babafkrn <babafkrn@gmail.com>
2018-06-08 02:49:08 +0530

    [FrontEnd]: Code changes for Exception Handling, Display user first name instead of user name in the nav bar drop down, Direct Login on Sucessfull Registration.

--- frontend/src/app/bs-navbar/bs-navbar.component.ts
+++ frontend/src/app/bs-navbar/bs-navbar.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login/login.service';
 import { RegisterService } from '../register/register.service';
 import { Observable } from 'rxjs';
+import { Constants } from "../models/constants";
+
 declare var $: any;
 
 @Component({
@@ -14,11 +16,8 @@ declare var $: any;
 })
 export class BsNavbarComponent {
 
-  returnUrl: string;
-  error:string;
-  errorMessage:string;
+  public errorMessage: string = ""; // to display the error message
   
-
   user: User = {
     username: null,
     password: null,
@@ -29,6 +28,7 @@ export class BsNavbarComponent {
     wallet_balance: null,
     _id:null
 };
+
 
 loginUser: User = {
   username: null,
@@ -41,33 +41,87 @@ loginUser: User = {
   _id:null
 };
 
-  constructor( public log: LoginService, public authService: AuthService,public register_service: RegisterService) {
+  /**
+   * Gets the instance.
+   * 
+   * @param loginService 
+   * @param authService 
+   * @param registerService 
+   * @param activateRoute 
+   */
+  constructor(
+    private loginService: LoginService, 
+    private authService: AuthService, 
+    private registerService: RegisterService, 
+    private activateRoute: ActivatedRoute) {
   }
 
-  login(newUser: User) {
-    console.log(newUser);
-    this.errorMessage = "";
-    this.log.login(newUser).subscribe((user) => {
-      //this.user = user;
-      this.authService.login(newUser);
+  /**
+   * This method authenticates the user in database.
+   * 
+   * @param newUser
+   */
+  public login(newUser: User): void {
+
+    // authenticate user
+    this.loginService.login(newUser).subscribe((user) => {
+  
+      // sets the return url in local storage
+      this.setReturnUrlInLocalStorage();
+      
+      // hide the modal window if the user authentication is successfull
       $("#myModal").modal("hide");
     }, (err) => {
-      console.log("=============" + err);
-      this.errorMessage = "Invalid Login Credentials...";
-      
+      console.log("Exception occured in login().bs-navbar.component.ts file", err);
+      this.errorMessage = "Invalid Credentials...";
     });
-  
   }
 
 
-  saveUser(newUser: User) {
-    this.register_service.saveUserIntoDB(newUser);
-    this.authService.login(newUser);
-    $("#myModal").modal("hide");
+  /**
+   * This function registers the user in database and makes the user login into the application.
+   * 
+   * @param newUser
+   */
+  public saveUser(newUser: User): void {
+
+    // register the user in database
+    this.registerService.saveUserIntoDB(newUser);
+
+    // fetch user details from database
+    this.loginService.login(newUser).subscribe((user) => {
+
+      // sets the return url in local storage
+      this.setReturnUrlInLocalStorage();
+
+      // hide the modal window if the user registration is successfull
+      $("#myModal").modal("hide");
+
+    }, (err) => {
+      console.log("Exception occured in saveUser().bs-navbar.component.ts file", err);
+      this.errorMessage = "We have encountered a Systm Exception...";
+    });
   }
 
-  logout(userDetails: User) {
+  /**
+   * This method sets the return url in the local storage.
+   */
+  private setReturnUrlInLocalStorage(): void {
+
+      let returnUrl = this.activateRoute.snapshot.queryParamMap.get(Constants.RETURN_URL) || '/';
+
+      // set in local storage
+      localStorage.setItem(Constants.RETURN_URL, returnUrl);
+  }
+
+  /**
+   * This functions logouts the user from the application.
+   * 
+   * @param userDetails 
+   */
+  public logout(userDetails: User): void {
+
+    // log out user from the application
     this.authService.logout(userDetails);
   }
-
 }
---
+++ frontend/src/app/models/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+    
+    public static get RETURN_URL(): string { return "returnUrl"; };
+
+}
